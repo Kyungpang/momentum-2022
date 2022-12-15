7fab6124f80f7c7a87350892d72ef46d85ecad22
Kyungpang <kkh3094@naver.com>
2022-12-15 22:09:43 +0900

    Complete : weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,17 @@ const API_KEY = "74a69d53a41542e5e164ad23ed631356";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const log = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lat}&appid=${API_KEY}&units=metric`;
-  fetch(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.main.temp}도 / ${data.weather[0].main}`;
+      city.innerText = data.name;
+    });
 }
+
 function onGeoError() {
   alert("Can't find you. No weather for you.");
 }
